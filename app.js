4171bfe1ffacc50262d1d1a69ef4541cab42a7c0
nataberger <natasha.berger@innew.la>
2024-01-11 11:44:16 -0300

    changed app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express();
 const path = require('path');
 const morgan = require('morgan');
+const cors = require('cors');
 const methodOverride = require('method-override');
 const mongoose = require('mongoose')
 require('dotenv').config();
@@ -12,56 +13,58 @@ const PORT = process.env.PORT
 
 // Database config
 const DATABASE_URL = process.env.DATABASE_URL;
+
+// deprecated
 const CONFIG = {
     useNewUrlParser: true,
     useUnifiedTopology: true
 };
 
 // Conection to database
-mongoose.connect(DATABASE_URL)
+
+const connection = async() => {
+    await mongoose.connect(DATABASE_URL)
+}
+connection();
 
 // Events for open/close connection
-
-mongoose.connection
-    .on('open', () => console.log('Conected to mongoose'))
+mongoose.connection.on('open', () => console.log('Conected to mongoose'))
     .on('close', () => console.log('Conection closed'))
     .on('error', (error) => console.log('Conection error', error))
 
-const { Schema, model } = mongoose
-
-const picturesSchema = new Schema({
-    text: String
-})
-
-const pictures = model("Pictures", picturesSchema)
-
-
 // Main app routes
-const main_routes = require('./src/routes/main_routes')
+const main_routes = require('./src/routes/main.routes')
 
 //template engine | ejs
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './src/views'));
 
 // middlewares
-app.use(express.urlencoded({extended: true})); 
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method')); // override for put and delete methods
-app.use(morgan('tiny')); //logging
+app.use(morgan('dev')); //logging
+app.use(cors());
 app.use(express.json());
+
 
 // serve static files
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public/styles')));
+app.use(express.static(path.join(__dirname, 'public/images')));
 app.use(express.static('public'));
 
 app.use('/', main_routes);
 app.use('/about', main_routes)
+app.use('/items', main_routes)
 
-app.get('/', async (req, res) => {
-    const pictures = await pictures.find({})
-    console.log('yes')
-    res.render('index.ejs', { todo })
-})
+// app.get('/', async (req, res) => {
+//     const pictures = await pictures.find({})
+//     console.log('yes')
+//     res.render('index.ejs', { pictures })
+// })
 
 app.listen(PORT, () => {
     console.log(`Servr started at port http://localhost:${PORT}`)
 })
+
+module.exports = app
