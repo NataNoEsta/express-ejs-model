7e3049c42f8226b37d69ca123e09f6f028319f18
nataberger <natasha.berger@innew.la>
2024-02-02 13:57:06 -0300

    feat: api controller

---
+++ src/controllers/api_controller.js
@@ -0,0 +1,27 @@
+const { Pictures } = require('../models/main_model.js');
+
+const items = ((req, res) => {
+  res.status(200).send('hey')
+})
+
+// const allitems = (async (req, res) => {
+//   const items = await Pictures.find().exec()
+//   res.json({items})
+// })
+
+const collection = (async (req, res) => {
+  try {
+    let { id } = req.params
+    const getall = !id ? await Pictures.find().exec() : await Pictures.findOne({ _id: id }).exec();
+    res.status(200).json({ getall })
+    console.log('fff',getall)
+  } catch (error) {
+    console.log(error)
+    res.status(404).json('error', error)
+  }
+})
+
+module.exports = {
+  items,
+  collection
+}
