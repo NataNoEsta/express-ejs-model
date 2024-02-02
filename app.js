9b8f96911db9eb17c274455a61ca89e1e7cef685
nataberger <natasha.berger@innew.la>
2024-02-02 14:44:06 -0300

    refactor: main_controller

--- app.js
+++ app.js
@@ -4,11 +4,10 @@ const path = require('path');
 const morgan = require('morgan');
 const cors = require('cors');
 const methodOverride = require('method-override');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const db = mongoose.connection;
 require('dotenv').config();
 
-
-// const PORT = 3000;
 const PORT = process.env.PORT
 
 // Database config
@@ -21,7 +20,6 @@ const CONFIG = {
 };
 
 // Conection to database
-
 const connection = async () => {
     await mongoose.connect(DATABASE_URL)
 }
@@ -29,12 +27,13 @@ connection();
 
 // Events for open/close connection
 mongoose.connection
-    .on('open', () => console.log('Conected to mongoose'))
+    .once('open', () => console.log('Conected to mongoose'))
     .on('close', () => console.log('Conection closed'))
     .on('error', (error) => console.log('Conection error', error))
 
 // Main app routes
-const main_routes = require('./src/routes/main.routes')
+const main_routes = require('./src/routes/main.routes.js');
+const api_routes = require('./src/routes/api.routes.js');
 
 //template engine | ejs
 app.set('view engine', 'ejs');
@@ -57,6 +56,10 @@ app.use(express.static('public'));
 app.use('/', main_routes);
 app.use('/about', main_routes)
 app.use('/items', main_routes)
+app.use('/items/:id', main_routes)
+app.use('/api/', api_routes)
+app.use('/api/collection', api_routes)
+app.use('/api/collection/:id', api_routes)
 
 // app.get('/', async (req, res) => {
 //     const pictures = await pictures.find({})
--- src/controllers/main_controller.js
+++ src/controllers/main_controller.js
@@ -23,29 +23,15 @@ const itemlist = (async(req, res) => {
             query = await Pictures.findById({ _id: id}).exec()
             res.render('pages/item', { data: query })
         }
-        // const query = !id ? await Pictures.find().exec() : await Pictures.findOne({ _id: id}).exec();
         console.log('aca',query)
-        // res.render('pages/items', { data: query })
     } catch (error) {
         return res.status(500).json(error)
     }
        
 })
 
-const item = (async(req, res) => {
-    try {
-        const { id } = req.params
-        const query = await Pictures.findOne({ _id: id }).exec();
-        console.log('acá', queryOne)
-        res.render('pages/item', {data: query} )
-    }catch (error) {
-        return res.status(500).json(error)
-    }
-})
-
 module.exports = {
     home,
     about,
-    itemlist,
-    item
+    itemlist
 }
